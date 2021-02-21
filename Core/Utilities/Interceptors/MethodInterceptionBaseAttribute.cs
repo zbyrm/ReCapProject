1d85f4c245afb07c7f1e7afcb4c2e71588bd55e4
zbyrm <ziyax@hotmail.com>
2021-02-22 00:05:51 +0300

    22.02.2021 AOP

---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Castle.DynamicProxy;//Atofac 
+
+namespace Core.Utilities.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }//oncelik
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+}
